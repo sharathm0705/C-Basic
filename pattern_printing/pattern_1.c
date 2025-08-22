eba0c5a82d5b879dc77ae61de8c977dce88d62c4
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-22 23:07:27 +0530

    Removed keys, updated pattern_1.c, added .gitignore and exaple.c

--- pattern_printing/pattern_1.c
+++ pattern_printing/pattern_1.c
@@ -194,10 +194,7 @@
         
 //     }printf("\n");
 //     }
-    
-    
-    
-// }
+    //}
 
 
 #include <stdio.h>
@@ -219,10 +216,68 @@ int main(){
             
         
     }printf("\n");
-    }
     
     
+    }for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+            
+        }
+        for(int k=1;k<=(n-i);k++){
+        printf(" ");}
+        for(int k=1;k<=(n-i);k++){
+        printf(" ");}
+        
+        
+        for(int j=1;j<=i;j++){
+            printf("*");
+            
+        
+    }printf("\n");
     
-}
+    }}
+
+
+// #include <stdio.h>
+// int main(){
+//     int n=4;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+            
+//         }
+//         for(int k=1;k<=(n-i);k++){
+//         printf(" ");}
+//         for(int k=1;k<=(n-i);k++){
+//         printf(" ");}
+        
+        
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+            
+        
+//     }printf("\n");
+    
+//     }
+//     for(int i=n-1;i>=1;i--){
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+            
+//         }
+//         for(int k=1;k<=(n-i);k++){
+//         printf(" ");}
+//         for(int k=1;k<=(n-i);k++){
+//         printf(" ");}
+        
+        
+//         for(int j=1;j<=i;j++){
+//             printf("*");
+            
+        
+//     }printf("\n");
+    
+    
+//     }}
+
 
 
