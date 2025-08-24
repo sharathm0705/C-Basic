e6deb71eb5933133229534371e0aff160e371c44
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-24 15:40:23 +0530

    added fibonacci

---
+++ loops/fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int n,first,second,third;
+    scanf("%d",&n);
+    if(n==1){
+        printf("0");
+    }else if(n==2){
+        printf("1");
+    }else{
+    first=0;
+    second=1;
+    for(int i=1;i<=n-2;i++){
+        third=first+second;
+        first=second;
+        second=third;
+        
+    }
+printf("%d\n",third);
+
+}
+    return 0;
+}
+
+
