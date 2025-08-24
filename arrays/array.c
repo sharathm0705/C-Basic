e8c60623905a251dcf9bac6781b5c54cc1dc9055
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-24 12:25:06 +0530

    added few problems on array

--- arrays/array.c
+++ arrays/array.c
@@ -190,26 +190,96 @@
 
 
 //To check wether all the numbers in an array is Palindrome :
+// #include <stdio.h>
+// int main(){
+//     int rev=0,flag=0;
+//     int a[]={123321,121,4334,261162,656};
+//     int n=sizeof(a)/sizeof(a[0]);
+    
+//     for(int i=0;i<n;i++){
+//      int temp=a[i],rev=0;
+//         while(temp>0){
+            
+//          rev=rev*10+temp%10;
+//          temp=temp/10;
+        
+//         }if(rev!=a[i]){
+//             flag=1;
+//             break;
+//          }
+//     }if(flag==0){
+//         printf("1");
+//     }else
+//     printf("0");
+//     return 0;
+// }
+
+
+
+
+
+//Given a positive integer x and an array arr of positive integers. We need to find the farthest index at which x is present. If any occurrence of x doesn't exist, then return -1.
+
+// Examples:
+
+// Input: arr[] = [7, 42, 5, 6, 42, 8, 7, 5, 3, 6, 7] and x = 7
+// Output: 11
+// Explanation: The last index contains value 7.
+// Input: arr[] = [1, 2, 2] and x = 1
+// Output: 1
+// Expplanation: The farthest index which contains value x is 1.
+//Program :
+// #include <stdio.h>
+// int main(){
+// int a[]={2,4,1,65,34,12,4,1,2};
+// int n=sizeof(a)/sizeof(a[0]);
+// int x;
+// scanf("%d",&x);
+// for(int i=n-1;i>=0;i--){
+//     if(a[i]==x){
+//         printf("%d",i+1);
+//         return 0;
+//     }
+// }
+//     printf("-1");
+    
+//}
+
+
+
+// A person needs to reach home by train but compulsively switches trains at every station. If they start with train arr1 at the first station, they will switch to train arr2 at the next station, and then switch back to train arr1 at the following station, and so on. Similarly, if they start with train arr2, they will switch to train arr1 at the next station, and continue alternating.
+
+// Given two arrays, arr1 and arr2, where:
+
+// arr1[i] represents the time taken by train arr1 at station i.
+// arr2[i] represents the time taken by train arr2 at station i.
+// Determine the minimum total time required to reach home starting from either train arr1 or train arr2, with the switching pattern maintained.
+
+// Examples:
+// Input: arr1 = [2, 1, 2], arr2 = [3, 2, 1]
+// Output: 5
+// Explanation: Starting with train arr2 yields the minimum total time of 5.
+// Input: arr1 = [1, 3, 1, 2] arr2 = [2, 2, 3, 1]
+// Output: 5
+// Explanation: Starting with train arr1 yields the minimum total time of 5.
+
+//Program:
 #include <stdio.h>
 int main(){
-    int rev=0,flag=0;
-    int a[]={123321,121,4334,261162,656};
-    int n=sizeof(a)/sizeof(a[0]);
-    
-    for(int i=0;i<n;i++){
-     int temp=a[i],rev=0;
-        while(temp>0){
-            
-         rev=rev*10+temp%10;
-         temp=temp/10;
-        
-        }if(rev!=a[i]){
-            flag=1;
-            break;
-         }
-    }if(flag==0){
-        printf("1");
-    }else
-    printf("0");
+    int arr1[]={2,1,2};
+    int arr2[]={3,2,1};
+    int sum1=arr1[0],sum2=arr2[0],even=0,odd=0;
+    int n=sizeof(arr1)/sizeof(arr1[0]);
+
+    for(int i=1;i<n;i++){
+        sum1+=i%2==0?arr1[i]:arr2[i];
+        sum2+=i%2==0?arr2[i]:arr1[i];
+
+    }
+    if(sum1<sum2)
+    printf("%d",sum1);
+    else
+    printf("%d",sum2);
+
     return 0;
 }
