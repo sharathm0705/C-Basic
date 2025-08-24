ee112c2013c530d2a67ac6c509a1fe6e9684b351
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-24 10:30:59 +0530

    added binary & linear search

--- arrays/array.c
+++ arrays/array.c
@@ -153,14 +153,14 @@
 //Binary Search :(Binary search is possible only in sorted array)
 #include <stdio.h>
 int main(){
-int a[]={2,3,45,8,9,14,21,35,45,5,68,74,87};
+int a[]={2,3,45,8,9,14,21,35,45,57,68,74,87};
 int n=sizeof(a)/sizeof(a[0]);
 int key,left=0,right=n-1,mid=0;
 scanf("%d",&key);
-for(int i=0;i<n;i++){
+while(left<=right){
     mid=(left+right)/2;
-    if(a[i]==key){
-        printf("%d\n",i);
+    if(a[mid]==key){
+        printf("%d\n",mid);
         return 0;
     }else if(a[mid]>key){
         right=mid-1;
