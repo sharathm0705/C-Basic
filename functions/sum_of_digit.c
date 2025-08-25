6e79dd84609a632d066b7adc1f3e50788a87b6bc
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-25 10:34:56 +0530

    add functions

---
+++ functions/sum_of_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void sum_of_digit();
+int main(){
+    int n;
+    scanf("%d",&n);
+    sum_of_digit(n);
+    return 0;
+}
+
+void sum_of_digit(int n){
+    int sum=0;
+    while(n>0){
+    sum=sum+n%10;
+    n=n/10;
+}
+printf("Total sum of Digit is %d\n",sum);
+}
