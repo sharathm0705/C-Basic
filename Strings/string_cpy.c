0dc94327ca2c9157a4c030cb9064f02da47283a6
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-26 10:30:28 +0530

    Added Strings

---
+++ Strings/string_cpy.c
@@ -0,0 +1,29 @@
+//To copy one string to another string :
+//Manual method :
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[]="abcde";
+    char str2[6];
+    for(int i=0;i<strlen(str1);i++)
+    str2[i]=str1[i];
+    str2[6]='\0';
+    printf("%s",str2);
+
+    return 0;
+}
+
+
+//Using Library function
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+//     char str1[]="abcde";
+// char str2[5];
+// strcpy(str2,str1);
+// printf("%s",str2);
+
+// return 0;
+// }
+
