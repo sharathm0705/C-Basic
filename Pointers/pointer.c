4864d6cc5cd9bbd1ffe7a2cdf014fc23cf04a9c3
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-27 14:27:57 +0530

    Pointers

--- Pointers/pointer.c
+++ Pointers/pointer.c
@@ -1,8 +1,21 @@
+// #include <stdio.h>
+// int main(){
+//     int a=5;
+//     int *ptr = &a;
+//     printf("%p",ptr);
+//     return 0;
+// }
+
+
+
+//Double and Triple Pointer :
 #include <stdio.h>
 int main(){
-    int a=5;
-    int *ptr = &a;
-    printf("%p",ptr);
+    int n=5;
+    int *p=&n;
+    int **q=&p;
+    int ***r=&q;
+    printf("%d",***r);
     return 0;
 }
 
