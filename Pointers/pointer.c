a8707f05b51d6658a117b265ecb475d9a1441b51
sharathm0705 <sharathmnayak2005@gmail.com>
2025-08-27 15:45:28 +0530

    Structures

--- Pointers/pointer.c
+++ Pointers/pointer.c
@@ -2,21 +2,36 @@
 // int main(){
 //     int a=5;
 //     int *ptr = &a;
-//     printf("%p",ptr);
+//     printf("%u",
+//         ptr);
 //     return 0;
 // }
 
 
 
-//Double and Triple Pointer :
+// Double and Triple Pointer :
+// #include <stdio.h>
+// int main(){
+//     int n=5;
+//     int *p=&n;
+//     int **q=&p;
+//     int ***r=&q;
+//     printf("%d",***r);
+//     return 0;
+// }
+
+
+
 #include <stdio.h>
+void fun(int *n);
 int main(){
     int n=5;
-    int *p=&n;
-    int **q=&p;
-    int ***r=&q;
-    printf("%d",***r);
+    fun(&n);
+    printf("%d",n);
     return 0;
+}
+void fun(int *n){
+    *n=6;
 }
 
 
