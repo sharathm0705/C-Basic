c17f5231e45395ac670bc6e9568d8073e8b366fb
sharathm0705 <sharathmnayak2005@gmail.com>
2026-07-18 11:55:30 +0530

    added selection sort

---
+++ Problems/selection_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int arr[] = {6,3,1,8,9,2};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    int temp;
+    for(int i = 0 ; i < len ; i++)
+    {
+        for(int j = 0 ; j < len-i-1 ; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    for(int i = 0 ; i < len ; i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+
+    return 0;
+}
